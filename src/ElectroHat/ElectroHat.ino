1171f9ab1b083524b7280590ccd3c5820571be29
Duane Northcutt <duane.northcutt@gmail.com>
2022-10-27 13:06:58 -0700

    Created.

---
+++ src/ElectroHat/ElectroHat.ino
@@ -0,0 +1,342 @@
+/***************************************************************************
+*
+* ElectroHat application
+* 
+* Drives eight 9' EL wires using the AFD Driver board, drives a ring of 16
+*  NeoPixels, and provides a web-server-based GUI.
+* 
+* N.B. This is insecure in that it passes the WiFi password in the clear -- you've been warned...
+*       (actually, it's slightly obfuscated, but essentially in the clear)
+*
+* WebSocket interface
+*  - From client to server:
+*   * {"applName": <String>, "libVersion": <String>, "ipAddr": <String>,
+*      "ssid": <String>, "passwd": <String>, "RSSI": <Int>, "el": <Boolean>,
+*      "randomSequence": <Boolean>, "sequenceNumber": <Int>, "sequenceSpeed": <Int>,
+*      "led": <Boolean>}
+*  - From server to client:
+*   * {"msgType": "el", "state": <Boolean>}
+*   * {"msgType": "sequence", "sequenceNumber: <Int>, sequenceSpeed: <Int>}
+*   * {"msgType": "saveConf", "ssid": document.getElementById("ssid").value,
+*      "passwd": <String>, "elState": <Boolean>, "randomSequence": <Boolean>,
+*      "sequenceNumber": <Int>, "sequenceSpeed": <Int>}
+*   * {"msgType": "randomSequence", "state": <Boolean>}
+**** TODO add Pixels stuff
+*
+* Notes:
+*  - I2C defaults: SDA=GPIO4 -> D2, SCL=GPIO5 -> D1
+*    * XIAO ESP32-C3: SDA: GPIO6/D4/pin5; SCL: GPIO7/D5/pin6
+*    * NodeMCU D1: SDA=GPIO4/D2/pin13; SCL=GPIO5/D1/pin14
+*  - connect to http://<ipaddr>/index.html for web interface
+*  - connect to http://<ipaddr>/update for OTA update of firmware
+* 
+* TODO
+*  - ?
+*
+****************************************************************************/
+
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#include "wifi.h"
+#include "sequences.h"
+
+#include <WiFiUtilities.h>
+#include <ConfigService.h>
+#include <WebServices.h>
+#include <ElWires.h>
+
+
+#define APPL_NAME           "ElectroHatCones"
+#define APPL_VERSION        "1.0.1"
+
+#define VERBOSE             1
+
+#define CONFIG_PATH         "/config.json"
+
+#define WIFI_AP_SSID        "ElectroHat"
+ 
+#define WEB_SERVER_PORT     80
+
+#define STD_WAIT            35
+
+#define STARTUP_EL_SEQUENCE 0
+#define STARTUP_EL_SPEED    100
+
+#define EH_HTML_PATH        "/index.html"
+#define EH_STYLE_PATH       "/style.css"
+#define EH_SCRIPTS_PATH     "/scripts.js"
+
+#define VALID_ENTRY(doc, key)    (doc.containsKey(key) && !doc[key].isNull())
+
+#define LED_PIN         15
+#define LED_COUNT       16
+#define LED_BRIGHTNESS  50  // Set BRIGHTNESS to about 1/5 (max = 255)
+
+
+typedef struct {
+    String         ssid;
+    String         passwd;
+    bool           elState;
+    bool           randomSequence;
+    unsigned short sequenceNumber;
+    unsigned short sequenceSpeed;
+    bool            ledState;
+} ConfigState;
+
+ConfigState configState = {
+    String(WLAN_SSID),
+    String(rot47(WLAN_PASS)),
+    true,
+    false,
+    STARTUP_EL_SEQUENCE,
+    STARTUP_EL_SPEED,
+    true
+};
+
+ElWires elWires;
+
+WebServices webSvcs(APPL_NAME, WEB_SERVER_PORT);
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void(* reboot)(void) = 0;
+
+void print(String str) {
+    if (VERBOSE) {
+        Serial.print(str);
+    }
+};
+
+void println(String str) {
+    if (VERBOSE) {
+        Serial.println(str);
+    }
+};
+
+String webpageProcessor(const String& var) {
+    if (var == "APPL_NAME") {
+        return (String(APPL_NAME));
+    } else if (var == "VERSION") {
+        return (String(APPL_VERSION));
+    } else if (var == "LIB_VERSION") {
+        return (webSvcs.libVersion);
+    } else if (var == "IP_ADDR") {
+        return (WiFi.localIP().toString());
+    } else if (var == "SSID") {
+        return (configState.ssid);
+    } else if (var == "RSSI") {
+        return (String(WiFi.RSSI()));
+    } else if (var == "WIFI_MODE") {
+        return (getWiFiMode());
+    } else if (var == "WIFI_AP_SSID") {
+        return (WIFI_AP_SSID);
+    } else if (var == "CONNECTION") {
+        return (WiFi.SSID());
+    } else if (var == "EL_STATE") {
+        return (configState.elState ? "ON" : "OFF");
+    } else if (var == "SEQUENCES_VERSION") {
+        return (elWires.libVersion);
+    } else if (var == "NUMBER_OF_SEQUENCES") {
+        return (String(elWires.numSequences()));
+    } else if (var == "LED_STATE") {
+        return (configState.ledState ? "ON" : "OFF");
+    }
+    return(String());
+};
+
+String webpageMsgHandler(const JsonDocument& wsMsg) {
+    if (VERBOSE) { //// TMP TMP TMP
+        Serial.println("MSG:");
+        serializeJsonPretty(wsMsg, Serial);
+    }
+    String msgType = String(wsMsg["msgType"]);
+    if (msgType.equalsIgnoreCase("query")) {
+        // NOP
+    } else if (msgType.equalsIgnoreCase("el")) {
+        configState.elState = wsMsg["state"];
+        if (!configState.elState) {
+            elWires.clear();
+        }
+    } else if (msgType.equalsIgnoreCase("sequence")) {
+        elWires.setSequence(wsMsg["sequenceNumber"], wsMsg["sequenceSpeed"]);
+        configState.sequenceNumber = elWires.sequenceNumber();
+        configState.sequenceSpeed = elWires.sequenceSpeed();
+    } else if (msgType.equalsIgnoreCase("randomSequence")) {
+        elWires.enableRandomSequence(wsMsg["state"]);
+        configState.randomSequence = wsMsg["state"];
+    } else if (msgType.equalsIgnoreCase("saveConf")) {
+        String ssidStr = String(wsMsg["ssid"]);
+        configState.ssid = ssidStr;
+        cs.configJsonDoc["ssid"] = ssidStr;
+        String passwdStr = String(wsMsg["passwd"]);
+        configState.passwd = passwdStr;
+        cs.configJsonDoc["passwd"] = passwdStr;
+
+        configState.elState = wsMsg["elState"];
+        cs.configJsonDoc["elState"] = wsMsg["elState"];
+        configState.randomSequence = wsMsg["randomSequence"];
+        cs.configJsonDoc["randomSequence"] = wsMsg["randomSequence"];
+        configState.sequenceNumber = wsMsg["sequenceNumber"];
+        cs.configJsonDoc["sequenceNumber"] = wsMsg["sequenceNumber"];
+        configState.sequenceSpeed = wsMsg["sequenceSpeed"];
+        cs.configJsonDoc["sequenceSpeed"] = wsMsg["sequenceSpeed"];
+    
+        serializeJsonPretty(cs.configJsonDoc, Serial);
+        cs.saveConfig();
+    } else if (msgType.equalsIgnoreCase("led")) {
+        configState.ledState = wsMsg["state"];
+        if (!configState.ledState) {
+            strip.show();
+        }
+    } else if (msgType.equalsIgnoreCase("reboot")) {
+        println("REBOOTING...");
+        reboot();
+    }
+
+    String msg = ", \"libVersion\": \"" + webSvcs.libVersion + "\"";
+    msg += ", \"ipAddr\": \"" + WiFi.localIP().toString() + "\"";
+    msg += ", \"ssid\": \"" + configState.ssid + "\"";
+    msg += ", \"passwd\": \"" + configState.passwd + "\"";
+    msg += ", \"RSSI\": " + String(WiFi.RSSI());
+    msg += ", \"el\": \"" + String(configState.elState ? "true" : "false") + "\"";
+    msg += ", \"randomSequence\": \"" + String(configState.randomSequence ? "true" : "false") + "\"";
+    msg += ", \"sequenceNumber\": " + String(configState.sequenceNumber) + "\"";
+    msg += ", \"sequenceSpeed\": " + String(configState.sequenceSpeed) + "\"";
+    msg += ", \"led\": \"" + String(configState.ledState ? "true" : "false");
+    Serial.println(msg);  //// TMP TMP TMP
+    return(msg);
+};
+
+WebPageDef webPage = {
+    EH_HTML_PATH,
+    EH_SCRIPTS_PATH,
+    EH_STYLE_PATH,
+    webpageProcessor,
+    webpageMsgHandler
+};
+
+
+void config() {
+    bool dirty = false;
+    cs.open(CONFIG_PATH);
+
+    unsigned short  sequenceNumber;
+    unsigned short  sequenceSpeed;
+
+    if (!VALID_ENTRY(cs.configJsonDoc, "ssid")) {
+        cs.configJsonDoc["ssid"] = configState.ssid;
+        dirty = true;
+    }
+    if (!VALID_ENTRY(cs.configJsonDoc, "passwd")) {
+        cs.configJsonDoc["passwd"] = configState.passwd;
+        dirty = true;
+    }
+    if (!VALID_ENTRY(cs.configJsonDoc, "elState")) {
+        cs.configJsonDoc["elState"] = configState.elState;
+        dirty = true;
+    }
+    if (!VALID_ENTRY(cs.configJsonDoc, "randomSequence")) {
+        cs.configJsonDoc["randomSequence"] = configState.randomSequence;
+        dirty = true;
+    }
+    if (!VALID_ENTRY(cs.configJsonDoc, "sequenceNumber")) {
+        cs.configJsonDoc["sequenceNumber"] = configState.sequenceNumber;
+        dirty = true;
+    }
+    if (!VALID_ENTRY(cs.configJsonDoc, "sequenceSpeed")) {
+        cs.configJsonDoc["sequenceSpeed"] = configState.sequenceSpeed;
+        dirty = true;
+    }
+    if (!VALID_ENTRY(cs.configJsonDoc, "ledState")) {
+        cs.configJsonDoc["ledState"] = configState.ledState;
+        dirty = true;
+    }
+    if (dirty) {
+        serializeJsonPretty(cs.configJsonDoc, Serial);
+        cs.saveConfig();
+    }
+
+    configState.ssid = cs.configJsonDoc["ssid"].as<String>();
+    configState.passwd = cs.configJsonDoc["passwd"].as<String>();
+    configState.elState = cs.configJsonDoc["elState"].as<bool>();
+    configState.randomSequence = cs.configJsonDoc["randomSequence"].as<bool>();
+    configState.sequenceNumber = cs.configJsonDoc["sequenceNumber"].as<unsigned int>();
+    configState.sequenceSpeed = cs.configJsonDoc["sequenceSpeed"].as<unsigned int>();
+    configState.ledState = cs.configJsonDoc["ledState"].as<bool>();
+    if (VERBOSE) {
+        println("Config File:");
+        serializeJsonPretty(cs.configJsonDoc, Serial);
+        cs.listFiles(CONFIG_PATH);
+        cs.printConfig();
+//        serializeJsonPretty(cs.configJsonDoc, Serial);
+        println("");
+    }
+};
+
+void initElWires() {
+    println("Init EL Wires");
+    elWires.clear();
+    elWires.setSequence(configState.sequenceNumber, configState.sequenceSpeed);
+    println("Number of Sequences: " + String(elWires.numSequences()));
+    println("Random Sequence Enabled: " + String(elWires.randomSequence() ? "Yes" : "No"));
+    println("Sequence Number: " + String(elWires.sequenceNumber()));
+    println("Sequence Speed: " + String(elWires.sequenceSpeed()));
+};
+
+void ledsRun() {
+    println("LEDs Run");
+}
+
+void setup() { 
+    delay(500);
+    Serial.begin(19200);
+    delay(500);
+    Serial.println("\nBEGIN");
+
+    //// FIXME TMP TMP TMP
+    if (false) {
+        // clear the local file system
+        cs.format();
+    }
+    if (true) {
+        Serial.println("Local Files:");
+        cs.listFiles("/");
+    }
+
+    config();
+
+    wiFiConnect(configState.ssid, rot47(configState.passwd), WIFI_AP_SSID);
+
+    if (!webSvcs.addPage(webPage)) {
+        Serial.println("ERROR: failed to add common page; continuing anyway");
+    }
+
+    initElWires();
+    Serial.println("EL wires function started");
+
+    webSvcs.updateClients();
+
+    strip.begin();
+    strip.show();  // turn off all pixels
+    strip.setBrightness(LED_BRIGHTNESS);
+    Serial.println("NeoPixels function started");
+
+    Serial.println("READY");
+};
+
+void loop() {
+    webSvcs.run();
+
+    if (configState.elState) {
+        elWires.run();
+    } else if (configState.ledState) {
+        ledsRun();
+        delay(1);
+    } else {
+        // MAGIC NUMBER: measured to approximate constant delay through loop
+        delayMicroseconds(100);
+    }
+    delay(1);
+};
---
+++ src/ElectroHat/data/scripts.js
@@ -0,0 +1,110 @@
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', onLoad);
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage;
+  }
+  function onOpen(event) {
+    console.log('Connection opened');
+    initView();
+  }
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+  }
+  function onLoad(event) {
+    initWebSocket();
+  }
+  function onMessage(event) {
+    var state;
+    var elem;
+    const msgObj = JSON.parse(event.data);
+    console.log("msgObj: " + JSON.stringify(msgObj));
+
+    document.getElementById("ssid").value = msgObj.ssid;
+
+    elem = document.getElementById("password");
+    if (msgObj.passwd != null) {
+      elem.value = rot47(msgObj.passwd);
+    } else {
+      elem.value = "";
+    }
+
+    var el = (msgObj.el == "true");
+    document.getElementById('elState').innerHTML = (el ? "ON" : "OFF");
+    document.getElementById('el').checked = el;
+
+    var rs = (msgObj.randomSequence == "true");
+    document.getElementById('randomSequence').checked = rs;
+    document.getElementById('sequenceNumber').disabled = rs;
+
+    elem = document.getElementById('sequenceNumber');
+    elem.value = msgObj.sequenceNumber;
+
+    elem = document.getElementById('sequenceSpeed');
+    elem.value = msgObj.sequenceSpeed;
+    document.getElementById("speed").innerHTML = parseInt(msgObj.sequenceSpeed);
+
+    var led = (msgObj.led == "true");
+    document.getElementById('ledState').innerHTML = (led ? "ON" : "OFF");
+    document.getElementById('led').checked = led;
+
+    document.getElementById("save").disabled = false;
+  }
+  function toggleCheckbox(element) {
+    element.innerHTML = (element.checked ? "ON" : "OFF");
+    var jsonMsg = JSON.stringify({"msgType": element.id, "state": element.checked});
+    websocket.send(jsonMsg);
+  }
+  function setSequence() {
+    var seqNum = document.getElementById("sequenceNumber").value;
+    var seqSpeed = document.getElementById("sequenceSpeed").value;
+    var jsonMsg = JSON.stringify({"msgType": "sequence", "sequenceNumber": seqNum, "sequenceSpeed": seqSpeed});
+    websocket.send(jsonMsg);
+  }
+  function saveConfiguration() {
+    var jsonMsg = JSON.stringify({"msgType": "saveConf",
+                                  "ssid": document.getElementById("ssid").value,
+                                  "passwd": rot47(document.getElementById("password").value),
+                                  "elState": document.getElementById('el').checked,
+                                  "randomSequence": document.getElementById("randomSequence").checked,
+                                  "sequenceNumber": document.getElementById('sequenceNumber').value,
+                                  "sequenceSpeed": document.getElementById('sequenceSpeed').value,
+                                  "ledState": document.getElementById('led').checked
+                                });
+    document.getElementById("save").disabled = true;
+    websocket.send(jsonMsg);
+  }
+  function toggleRandomSequence() {
+    var randomSeq = document.getElementById("randomSequence").checked;
+    document.getElementById("sequenceNumber").disabled = randomSeq;
+    var jsonMsg = JSON.stringify({"msgType": "randomSequence", "state": randomSeq});
+    websocket.send(jsonMsg);
+  }
+  function escapeHTML(s) {
+    return s.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/'/g, '&apos;')
+        .replace(/"/g, '&quot;')
+        .replace(/\//g, '&sol;');
+  }
+  function rot13(str) {
+    return str.split('').map(char => String.fromCharCode(char.charCodeAt(0) + (char.toLowerCase() < 'n' ? 13 : -13))).join('');
+  }
+  function rot47(x) {
+    var s = [];
+    for(var i = 0; (i < x.length); i++) {
+      var j = x.charCodeAt(i);
+      if ((j >= 33) && (j <= 126)) {
+        s[i] = String.fromCharCode(33 + ((j + 14) % 94));
+      } else {
+        s[i] = String.fromCharCode(j);
+      }
+    }
+    return s.join('');
+  }
