1392f5fc3ea208184d6abc012f32dd8fcbb32040
Duane Northcutt <duane.northcutt@gmail.com>
2022-10-29 13:27:52 -0700

    Checkpoint.

--- libraries/NeoPixelRing/src/NeoPixelRing.h
+++ libraries/NeoPixelRing/src/NeoPixelRing.h
@@ -14,27 +14,69 @@
 #define LED_COUNT           16
 #define DEF_LED_BRIGHTNESS  50  // max = 255
 
+#define PATTERN_FUNC(func)  [](NeoPixelRing* npr) { return npr->func();}
 
-#define LED_TEST_ALL_WHITE  0
-#define LED_TEST_ALL_RED    1
-#define LED_TEST_ALL_GREEN  2
-#define LED_TEST_ALL_BLUE   3
-#define LED_TEST_ALL_OFF    4
-#define LED_TEST_ALL_BLINK  5
+class NeoPixelRing;
+using PatternFunc = void (*)(NeoPixelRing*);
+
+typedef struct {
+    PatternFunc func;
+    char *name;
+} Patterns;
 
 
 class NeoPixelRing {
 public:
     String libVersion = NEO_PIXEL_RING_VERSION;
+
+    enum LED_TEST {
+        ALL_WHITE = 0,
+        ALL_RED   = 1,
+        ALL_GREEN = 2,
+        ALL_BLUE  = 3,
+        ALL_OFF   = 4,
+        ALL_BLINK = 5
+    };
+
     NeoPixelRing();
     NeoPixelRing(byte brightness);
-    void run();
     void test(byte test);
+    void clear();
+    void run();
+    uint32_t makeColor(byte r, byte g, byte b);
+    void setColor(uint32_t color);
+    uint32_t getColor();
+    void setDelay(uint32_t delay);
+    uint32_t getDelay();
+    byte getNumPatterns();
+    byte getPatternNames(char *namePtrs[], byte number);
+    byte getSelectedPattern();
+    bool selectPattern(byte patternNum);
 
 private:
     Adafruit_NeoPixel *_ring = new Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+    static byte const _NUM_PATTERNS = 3;
+
+    byte _pixelNum = 0;
+    uint32_t _color = 0x000000;
+    uint32_t _delay = 0;
+    byte _patternNum = 0;
+    unsigned long _nextRunTime = 0;
+    uint32_t _firstPixelHue = 0;
+    unsigned long _loopCnt = 0;
+
     void _create(byte brightness);
+    void _colorWipe();
+    void _marquee();
+    void _rainbow();
+
+//    const PatternFunc _patterns[_NUM_PATTERNS] = {
+    const Patterns _patterns[_NUM_PATTERNS] = {
+        {PATTERN_FUNC(_colorWipe), "Color Wipe"},
+        {PATTERN_FUNC(_marquee), "Marquee"},
+        {PATTERN_FUNC(_rainbow), "Rainbow"}
+    };
 };
 
 #include "NeoPixelRing.hpp"
--- libraries/NeoPixelRing/src/NeoPixelRing.hpp
+++ libraries/NeoPixelRing/src/NeoPixelRing.hpp
@@ -4,11 +4,11 @@
 
 NeoPixelRing::NeoPixelRing() {
     _create(DEF_LED_BRIGHTNESS);
-}
+};
 
 NeoPixelRing::NeoPixelRing(byte brightness) {
     _create(brightness);
-}
+};
 
 void NeoPixelRing::_create(byte brightness) {
     _ring->begin();
@@ -16,41 +16,34 @@ void NeoPixelRing::_create(byte brightness) {
     _ring->show();  // turn off all pixels
     _ring->setBrightness(brightness);
     _ring->setPixelColor(1, 255, 0, 255);
-}
+};
 
 void NeoPixelRing::test(byte test) {
     uint32_t c;
 
     switch (test) {
-    case LED_TEST_ALL_WHITE:
+    case NeoPixelRing::ALL_WHITE:
         c = _ring->Color(255, 255, 255);
-        Serial.println("WHT: 0x" + String(c, HEX));
         _ring->fill(c, 0, LED_COUNT);
-        Serial.println("XXX");
         _ring->show();
-        Serial.println("YYY");
         break;
-    case LED_TEST_ALL_RED:
-        Serial.println("RED");
+    case NeoPixelRing::ALL_RED:
         _ring->fill(_ring->Color(255, 0, 0), 0, LED_COUNT);
         _ring->show();
         break;
-    case LED_TEST_ALL_GREEN:
-        Serial.println("GRN");
+    case NeoPixelRing::ALL_GREEN:
         _ring->fill(_ring->Color(0, 255, 0), 0, LED_COUNT);
         _ring->show();
         break;
-    case LED_TEST_ALL_BLUE:
-        Serial.println("BLU");
+    case NeoPixelRing::ALL_BLUE:
         _ring->fill(_ring->Color(0, 0, 255), 0, LED_COUNT);
         _ring->show();
         break;
-    case LED_TEST_ALL_OFF:
-        Serial.println("OFF");
+    case NeoPixelRing::ALL_OFF:
         _ring->clear();
         _ring->show();
         break;
-    case LED_TEST_ALL_BLINK:
+    case NeoPixelRing::ALL_BLINK:
         int i;
         uint32_t c[LED_COUNT];
         for (i = 0; (i < LED_COUNT); i++) {
@@ -70,5 +63,107 @@ void NeoPixelRing::test(byte test) {
         Serial.println("Invalid test: " + String(test));
         return;
     }
-    Serial.println("ZZZ");
+};
+
+void NeoPixelRing::clear() {
+    _ring->clear();
+};
+
+// Fill pixels sequentially with the selected color, with the selected delay
+//  time (in msec) between each illumination.
+// Doesn't clear the pixels first.
+void NeoPixelRing::_colorWipe() {
+    _nextRunTime = millis() + _delay;
+    if (_pixelNum == 0) {
+        _ring->clear();
+    }
+    _ring->setPixelColor(_pixelNum, _color);
+    _ring->show();
+};
+
+// Movie-marquee-like chasing rainbow lights that precess
+void NeoPixelRing::_marquee() {
+    _nextRunTime = millis() + _delay;
+    _ring->clear();
+    int n = _ring->numPixels();
+    for (int i = (_loopCnt % 3); (i < n); i += 3) {
+        int hue = _firstPixelHue + i * 65536L / n;
+        uint32_t rgbColor = _ring->gamma32(_ring->ColorHSV(hue));
+//        Serial.println("> " + String(i) + ", 0x" + String(rgbColor, HEX));
+        _ring->setPixelColor(i, rgbColor);
+        _ring->show();
+    }
+    _firstPixelHue += 256;
+};
+
+// Loop through color wheel over all pixels.  Just changes Hue value and keeps
+//  Saturation and Brightness at max (i.e., 255).  Bumps starting pixel's Hue
+//  by 256 each iteration.
+void NeoPixelRing::_rainbow() {
+    _nextRunTime = millis() + _delay;
+    _ring->clear();
+    _ring->rainbow(_firstPixelHue);
+    _ring->show();
+    _firstPixelHue += 256;
+};
+
+void NeoPixelRing::run() {
+    unsigned long now = millis();
+    if (now > _nextRunTime) {
+        Serial.println("Warning: " + String(now - _nextRunTime) + " msec late");
+    } else {
+        if (_nextRunTime > now) {
+            delay(_nextRunTime - now);
+        }
+    }
+    _patterns[_patternNum].func(this);
+    _loopCnt += 1;
+    _pixelNum = _loopCnt % _ring->numPixels();
+};
+
+uint32_t NeoPixelRing::makeColor(byte r, byte g, byte b) {
+    return _ring->Color(r, g, b);
+};
+
+void NeoPixelRing::setColor(uint32_t color) {
+    _color = color;
+};
+
+uint32_t NeoPixelRing::getColor() {
+    return _color;
+};
+
+void NeoPixelRing::setDelay(uint32_t delay) {
+    _delay = delay;
+};
+
+uint32_t NeoPixelRing::getDelay() {
+    return _delay;
+};
+
+byte NeoPixelRing::getNumPatterns() {
+    return _NUM_PATTERNS;
+};
+
+//// FIXME use the right kind of args to capture both dims
+byte NeoPixelRing::getPatternNames(char *namePtrs[], byte number) {
+    if (number < _NUM_PATTERNS) {
+        return -1;
+    }
+    for (int i = 0; (i < _NUM_PATTERNS); i++) {
+        namePtrs[i] = _patterns[i].name;
+    }
+    return _NUM_PATTERNS;
+};
+
+byte NeoPixelRing::getSelectedPattern() {
+    return _patternNum;
+};
+
+bool NeoPixelRing::selectPattern(byte patternNum) {
+    if (patternNum >= _NUM_PATTERNS) {
+        return true;
+    }
+    _patternNum = patternNum;
+    return false;
 };
