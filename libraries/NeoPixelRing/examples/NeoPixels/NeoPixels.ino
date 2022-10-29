8c658198979462e2c2136bafe42f8634817a453e
Duane Northcutt <duane.northcutt@gmail.com>
2022-10-29 14:13:50 -0700

    Added colorfill pattern and rotated colors.

--- libraries/NeoPixelRing/examples/NeoPixels/NeoPixels.ino
+++ libraries/NeoPixelRing/examples/NeoPixels/NeoPixels.ino
@@ -14,8 +14,9 @@
 #define DEF_LED_BRIGHTNESS  50  // max = 255
 
 #define COLOR_WIPE  0
-#define MARQUEE     1
-#define RAINBOW     2
+#define COLOR_FILL  1
+#define MARQUEE     2
+#define RAINBOW     3
 
 
 uint32_t loopCnt = 0;
@@ -63,27 +64,51 @@ void setup() {
 }
 
 int p = 0;
+
+uint32_t colors[] = {
+    ring.makeColor(0, 0, 255),
+    ring.makeColor(0, 255, 0),
+    ring.makeColor(255, 0, 0),
+    ring.makeColor(255, 0, 255),
+    ring.makeColor(255, 255, 0),
+    ring.makeColor(255, 255, 255)
+};
+
+int numColors = (sizeof(colors) / 4);
+
+int c = 0;
+
 void loop() {
-    if ((loopCnt % 256) == 0) {
+    int d;
+
+    if ((loopCnt % 128) == 0) {
         p = (p + 1) % 3;
         Serial.println("P: " + String(p) + ", Loop: " + String(loopCnt));
         ring.clear();
+        c += 1;
     }
+
     ring.selectPattern(p);
-    int d;
+
     switch (p) {
         case COLOR_WIPE:
             d = 50;
+            ring.setColor(colors[c % numColors]);
+            break;
+        case COLOR_FILL:
+            d = 75;
+            ring.setColor(colors[loopCnt % numColors]);
             break;
         case MARQUEE:
             d = 60;
             break;
         case RAINBOW:
-            d = 50;
+            d = 75;
             break;
         default:
             d = 10;
     }
+
     ring.setDelay(d);
     ring.run();
     loopCnt += 1;
